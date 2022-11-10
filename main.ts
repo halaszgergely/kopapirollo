c2cc0ebe2a5022d1119412e85decb69be034d603
halaszgergely <114067887+halaszgergely@users.noreply.github.com>
2022-11-10 09:39:28 +0100

    Kezdőfájlok a MakeCode projekthez

---
+++ main.ts
@@ -0,0 +1,35 @@
+let szam = 0
+input.onButtonPressed(Button.A, function () {
+    basic.clearScreen()
+})
+input.onGesture(Gesture.Shake, function () {
+    szam = randint(1, 3)
+    if (szam == 1) {
+        basic.showLeds(`
+            . # # # .
+            # # # # #
+            # # # # #
+            # # # # #
+            . # # # .
+            `)
+    } else if (szam == 2) {
+        basic.showLeds(`
+            # # . . #
+            # # . # .
+            . . # . .
+            # # . # .
+            # # . . #
+            `)
+    } else if (szam == 3) {
+        basic.showLeds(`
+            # # # # #
+            # # # # #
+            # # # # #
+            # # # # #
+            # # # # #
+            `)
+    }
+})
+input.onButtonPressed(Button.B, function () {
+    basic.clearScreen()
+})
